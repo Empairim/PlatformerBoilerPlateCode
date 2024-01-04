44727c84e9dd447194700ea4e275797fc6660810
Empairim <titusfaulknertull@yahoo.com>
2024-01-03 20:02:56 -0500

    set up player and game class, game will create instance of player auto

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// alot of files so gonna have a load function before running anything
 window.addEventListener("load", function () {
   //canvas setup
   const canvas = document.getElementById("canvas");
@@ -8,9 +9,39 @@ window.addEventListener("load", function () {
   class InputHandler {}
   class Projectile {}
   class Particle {}
-  class Player {}
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0; //vertical movement
+    }
+    update() {
+      this.y += this.speedY;
+    }
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
   class Layer {}
   class Background {}
   class UI {}
-  class Game {}
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this); // This allows you to have multiple instances of the Game class, each with its own set of players, and they won't interfere with each other.
+    }
+    update() {
+      this.player.update();
+    }
+    draw() {
+      this.player.draw(context); //it take this param above so has to here
+    }
+  }
+  const game = new Game(canvas.width, canvas.height);
+  // animation/game loop
+  function animate() {}
 });
