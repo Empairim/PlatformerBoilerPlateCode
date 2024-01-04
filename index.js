0c4839525766490bdd5fb39c9bff3f0d42119666
Empairim <titusfaulknertull@yahoo.com>
2024-01-03 21:10:58 -0500

    added input handler and movement controls to player class as well as key array

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ window.addEventListener("load", function () {
   canvas.width = 1500;
   canvas.height = 500;
 
-  class InputHandler {}
+  // Improved version of my player handler from last game
+  class InputHandler {
+    constructor(game) {
+      this.game = game;
+      window.addEventListener("keydown", (event) => {
+        //so key is only added once to keys array
+        if (
+          (event.key === "ArrowUp" || event.key === "ArrowDown") &&
+          this.game.keys.indexOf(event.key) === -1
+        ) {
+          this.game.keys.push(event.key);
+        }
+        console.log(this.game.keys);
+      });
+      window.addEventListener("keyup", (event) => {
+        if (this.game.keys.indexOf(event.key) > -1) {
+          this.game.keys.splice(this.game.keys.indexOf(event.key), 1);
+        }
+        console.log(this.game.keys);
+      });
+    }
+  }
   class Projectile {}
   class Particle {}
   class Player {
@@ -17,8 +38,13 @@ window.addEventListener("load", function () {
       this.x = 20;
       this.y = 100;
       this.speedY = 0; //vertical movement
+      this.maxSpeed = 5;
     }
     update() {
+      if (this.game.keys.includes("ArrowUp")) this.speedY = -this.maxSpeed;
+      else if (this.game.keys.includes("ArrowDown"))
+        this.speedY = this.maxSpeed;
+      else this.speedY = 0;
       this.y += this.speedY;
     }
     draw(context) {
@@ -33,15 +59,23 @@ window.addEventListener("load", function () {
       this.width = width;
       this.height = height;
       this.player = new Player(this); // This allows you to have multiple instances of the Game class, each with its own set of players, and they won't interfere with each other.
+      this.input = new InputHandler(this);
+      this.keys = []; //saves all key presses and allows me to pass it to the player class as well
     }
     update() {
       this.player.update();
     }
-    draw() {
+    draw(context) {
       this.player.draw(context); //it take this param above so has to here
     }
   }
   const game = new Game(canvas.width, canvas.height);
   // animation/game loop
-  function animate() {}
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // will clear the canvas b4 loop
+    game.update();
+    game.draw(ctx); // this is telling where we want it drawn then passes it back to the game/player class draw methods
+    requestAnimationFrame(animate);
+  }
+  animate();
 });
